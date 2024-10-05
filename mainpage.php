8fa6fca68feebc0e3fb51ba0655f1bec40a53f8c
AvaHtm <its.avictor81@gmail.com>
2024-10-05 09:50:43 +0330

    .

---
+++ mainpage.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body>
+<div class="container">
+<table class="table table-hover">
+    <thead>
+    <tr class="table table-primary">
+        <th scope="col">NO.</th>
+        <th scope="col">Category</th>
+        <th scope="col">Time Taken</th>
+        <th scope="col">score</th>
+        <th scope="col">status</th>
+        <th scope="col">open</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th scope="row">1</th>
+        <td>bootstrap Container</td>
+        <td>0 s</td>
+        <td>0/6</td>
+        <td>
+            <img src="images%20(1).png" alt="Bootstrap" width="40" height="40">
+        </td>
+        <td>
+            <img src="images.png" alt="Bootstrap" width="40" height="40">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row">2</th>
+        <td>bootstrap colors and images</td>
+        <td>0 s</td>
+        <td>0/6</td>
+        <td>
+            <img src="images%20(1).png" alt="Bootstrap" width="40" height="40">
+        </td>
+        <td>
+            <img src="images.png" alt="Bootstrap" width="40" height="40">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row">3</th>
+        <td>bootstrap table and alerts</td>
+        <td>0 s</td>
+        <td>0/6</td>
+        <td>
+            <img src="images%20(1).png" alt="Bootstrap" width="40" height="40">
+        </td>
+        <td>
+            <img src="images.png" alt="Bootstrap" width="40" height="40">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row">4</th>
+        <td>bootstrao buttons, badges and progressbar</td>
+        <td>0 s</td>
+        <td>0/8</td>
+        <td>
+            <img src="images%20(1).png" alt="Bootstrap" width="40" height="40">
+        </td>
+        <td>
+            <img src="images.png" alt="Bootstrap" width="40" height="40">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row">5</th>
+        <td>bootstrap spinner and pagination</td>
+        <td>0 s</td>
+        <td>0/6</td>
+        <td>
+            <img src="images%20(1).png" alt="Bootstrap" width="40" height="40">
+        </td>
+        <td>
+            <img src="images.png" alt="Bootstrap" width="40" height="40">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row">6</th>
+        <td>bootstrap list group and cards</td>
+        <td>0 s</td>
+        <td>0/5</td>
+        <td>
+            <img src="images%20(1).png" alt="Bootstrap" width="40" height="40">
+        </td>
+        <td>
+            <img src="images.png" alt="Bootstrap" width="40" height="40">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row">7</th>
+        <td>bootstrap dropdowns and collapse</td>
+        <td>0 s</td>
+        <td>0/4</td>
+        <td>
+            <img src="images%20(1).png" alt="Bootstrap" width="40" height="40">
+        </td>
+        <td>
+            <img src="images.png" alt="Bootstrap" width="40" height="40">
+        </td>
+    </tr>
+    <tr>
+        <th scope="row">8</th>
+        <td>bootstrap navs and navbar</td>
+        <td>0 s</td>
+        <td>0/5</td>
+        <td>
+            <img src="images%20(1).png" alt="Bootstrap" width="40" height="40">
+        </td>
+        <td>
+            <img src="images.png" alt="Bootstrap" width="40" height="40">
+        </td>
+    </tr>
+    </tbody>
+</table>
+<script src="js/bootstrap.bundle.min.js" ></script>
+</div>
+</body>
+</html>
